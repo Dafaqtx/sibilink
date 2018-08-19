89f6327fbf20c9e61ca13d41262cb0a474e84276
Alexander Kuchuk <dafaqtx@gmail.com>
2018-08-19 15:44:58 +0600

    init project

---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+var syntax        = 'sass'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		del           = require('del'),
+		imagemin      = require('gulp-imagemin'),
+		pngquant      = require('imagemin-pngquant'),
+		cache         = require('gulp-cache'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync');
+		csscomb       = require('gulp-csscomb');
+		csso          = require('gulp-csso');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({
+		includePaths: require('node-normalize-scss').includePaths, 
+		outputStyle: 'expanded' 
+	}).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(csscomb())
+	.pipe(csso())
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } }))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js',
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify())
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'],
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'],
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browserSync.reload)
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+gulp.task('img', function() {
+	return gulp.src('app/img/**/*')
+		.pipe(cache(imagemin({
+		// .pipe(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))/**/)
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', ['clean', 'img', 'styles', 'js'], function() {
+
+	var buildCss = gulp.src([ 
+		'app/css/main.min.css'
+		])
+	.pipe(gulp.dest('dist/css'))
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'))
+
+	var buildJs = gulp.src('app/js/**/*')
+	.pipe(gulp.dest('dist/js'))
+
+	var buildHtml = gulp.src('app/*.html')
+	.pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+})
+
+gulp.task('default', ['watch']);
