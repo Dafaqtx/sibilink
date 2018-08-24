868ebda1fb6e83d56076e9e954242e8fe1da2c4e
Alexander Kuchuk <dafaqtx@gmail.com>
2018-08-24 16:25:53 +0600

    add products pages + login page + product header

---
+++ app/650din.php
@@ -0,0 +1,193 @@
+<?php include('header-product.php') ?>
+
+<section class="choice">
+	<div class="container column">
+		<header class="flex">
+			<div class="choice__title"></div>
+			<div class="choice__desc text-right">GSM модуль управления шлагбаумом (воротами) с телефона. Открытие шлагбаума бесплатным звонком.
+				Совместим с любыми типами ворот и шлагбаумов.</div>
+		</header>
+		<div class="choice__content flex">
+			<div class="choice__product">
+				<div class="choice__img">
+					<img src="img/products/650din.png" alt="ДОМОВОЙ IP 650 DIN">
+				</div>
+				<!-- /.choice__img -->
+
+				<div class="choice__details flex">
+					<div class="choice__price">от <span class="choice__price--cost">2500</span> руб.</div>
+					<!-- /.choice__price -->
+
+					<button class="choice__btn">Купить</button>
+					<!-- /.choice__btn -->
+
+				</div>
+				<!-- /.choice__details -->
+
+			</div>
+			<!-- /.choice__product -->
+
+			<div class="choice__about" style="height: max-content; padding-bottom: 20px;">
+				<h3 class="choice__name">"ДОМОВОЙ IP " 650 DIN</h3>
+				<div class="choice__text">
+					<p>Контроллер GSM Ключ "ДОМОВОЙ" предназначен для управления доступом. GSM-ключ получает входящий вызов, после чего проверяет номер с которого поступил звонок по своей базе данных. Если абонент имеет необходимые разрешения, на устройство доступа подается сигнал на разрешение доступа.</p>
+
+					<p>Благодаря этим устройствам, вы легко можете осуществлять управление воротами с мобильного телефона. Точно также, со смартфона, можно управлять открытием-закрытием шлагбаумов, замков калиток и дверей, оснащенных GSM ключем "ДОМОВОЙ"</p>
+				</div>
+				<!-- /.choice__text -->
+			</div>
+			<!-- /.choice__about -->
+
+		</div>
+		<!-- /.choice__content -->
+
+	</div>
+</section>
+<!-- /.choice -->
+
+<section class="sertificate">
+	<div class="container column">
+		<header class="flex">
+			<h2 class="title sertificate__title">Сертификаты</h2>
+			<div class="sertificate__desc">
+				Вся наша продукция имеет необходимые сертификаты и соответствует техническим нормам производства.
+			</div>
+		</header>
+		<div class="sertificate__items js-sertificate-slider flex">
+			<div class="sertificate__item">
+				<div class="sertificate__img"><img src="img/sertificate/sertificate.jpg" alt=""></div>
+			</div>
+			<div class="sertificate__item">
+				<div class="sertificate__img"><img src="img/sertificate/sertificate.jpg" alt=""></div>
+			</div>
+			<div class="sertificate__item">
+				<div class="sertificate__img"><img src="img/sertificate/sertificate.jpg" alt=""></div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- /.sertificate -->
+
+<section class="call-back">
+	<div class="container column">
+		<header class="flex">
+			<h2 class="call-back__title">Получите бесплатную консультацию</h2>
+			<div class="call-back__text">Если вам требуется консультация, просто оставьте заявку 
+				и наш специалист свяжется с вами в ближайшее время!</div>
+		</header>
+		<form action="" class="call-back__form flex" method="post">
+			<input type="text" class="call-back__input" placeholder="Введите ваше Имя">
+			<input type="text" class="call-back__input" placeholder="Ваш номер телефона">
+			<button class="call-back__submit" type="submit">Отправить заявку</button>
+		</form>
+	</div>
+</section>
+<!-- /.call-back -->
+
+<section class="reviews">
+	<div class="container column">
+		<header>
+			<h2 class="title reviews__title">Отзывы клиентов</h2>
+		</header>
+		<div class="reviews__items js-reviews-slider">
+
+			<div class="reviews__item">
+				<div class="reviews__content flex flex__align-middle">
+					<div class="reviews__img">
+							<img src="img/reviews/avatar.png" alt="Иван Иванов">
+						</div>
+						<div class="reviews__info">
+							<div class="reviews__header flex flex__align-middle">
+								<div class="reviews__name">Иван Иванов</div>
+								<time class="reviews__date">25.08.2017</time>
+							</div>
+							<div class="reviews__text">
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p>
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p> 
+							</div>
+						</div>
+				</div>
+			</div>
+			<!-- /.reviews__item -->
+
+			<div class="reviews__item">
+				<div class="reviews__content flex flex__align-middle">
+					<div class="reviews__img">
+							<img src="img/reviews/avatar.png" alt="Иван Иванов">
+						</div>
+						<div class="reviews__info">
+							<div class="reviews__header flex flex__align-middle">
+								<div class="reviews__name">Иван Иванов</div>
+								<time class="reviews__date">25.08.2017</time>
+							</div>
+							<div class="reviews__text">
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p>
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p> 
+							</div>
+						</div>
+				</div>
+			</div>
+			<!-- /.reviews__item -->
+
+			<div class="reviews__item">
+				<div class="reviews__content flex flex__align-middle">
+					<div class="reviews__img">
+							<img src="img/reviews/avatar.png" alt="Иван Иванов">
+						</div>
+						<div class="reviews__info">
+							<div class="reviews__header flex flex__align-middle">
+								<div class="reviews__name">Иван Иванов</div>
+								<time class="reviews__date">25.08.2017</time>
+							</div>
+							<div class="reviews__text">
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p>
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p> 
+							</div>
+						</div>
+				</div>
+			</div>
+			<!-- /.reviews__item -->
+
+			<div class="reviews__item">
+				<div class="reviews__content flex flex__align-middle">
+					<div class="reviews__img">
+							<img src="img/reviews/avatar.png" alt="Иван Иванов">
+						</div>
+						<div class="reviews__info">
+							<div class="reviews__header flex flex__align-middle">
+								<div class="reviews__name">Иван Иванов</div>
+								<time class="reviews__date">25.08.2017</time>
+							</div>
+							<div class="reviews__text">
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p>
+								<p class="reviews__text--paragraph">
+									Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем. Текст отзывы полностью. Сейчас здесь написан текст для примера. И скопирован несколько раз, для того чтобы обозначить объем.
+								</p> 
+							</div>
+						</div>
+				</div>
+			</div>
+			<!-- /.reviews__item -->
+
+		</div>
+		<!-- /.reviews__items -->
+
+	</div>
+</section>
+<!-- /.reviews -->
+
+<?php include('footer.php') ?>
